3c3139f494f1f0a49d1610be3db4692e4dc9bf7b
Md Ataullah <ataullahm100@gmail.com>
2024-06-21 00:10:06 +0600

    Appintment form added

--- Client-Side/src/pages/Home/Home.jsx
+++ Client-Side/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { FiPhoneCall } from 'react-icons/fi';
 import useLang from '../../Hooks/useLang';
 import BannerAppiontmentFrom from '../../Utility/BannerAppiontmentFrom';
 import { Banner } from '../../components/Banner/Banner';
@@ -9,34 +10,41 @@ const Home = () => {
       <div className="relative">
         <Banner />
 
-        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-11/12 mx-auto h-full flex items-center gap-5">
-          <div className="h-64 w-1/2 border-slate-200 rounded-md pl-8">
-            <h1 className="text-6xl text-slate-50 font-bold">
-              {lang ? (
-                <span className="text-5xl leading-relaxed tracking-wider">
-                  সেরা <br /> মানের <span className="text-pClr">দাঁতের </span>
-                  চিকিৎসা।
+        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-11/12 mx-auto h-full flex flex-col md:flex-row items-center gap-5 text-center md:text-left">
+          <div className="min-h-64 w-full md:w-1/2 border-slate-200 rounded-md px-5 md:pl-8 flex items-center justify-center md:justify-start pt-12 md:pt-4">
+            <div className="space-y-3">
+              <h1 className="text-4xl md:text-6xl text-slate-50 font-bold">
+                {lang ? (
+                  <span className="text-3xl md:text-5xl md:leading-relaxed tracking-wider">
+                    সেরা মানের <br /> <span className="text-pClr">দাঁতের </span>
+                    চিকিৎসা।
+                  </span>
+                ) : (
+                  <span className="md:leading-snug">
+                    Best Quality <br />
+                    <span className="text-pClr">Dental </span> Treatment.
+                  </span>
+                )}
+              </h1>
+              <p className="text-slate-200">
+                {lang ? (
+                  <span className="">
+                    গত অর্ধ-যুগ ধরে আমরাই দিয়ে আসছি বাংলাদেশের সেরা ডেন্টাল
+                    সেবা।
+                  </span>
+                ) : (
+                  'We have been providing the best dental services in Bangladesh for the past half-century.'
+                )}
+              </p>
+              <span className="text-xl font-bold text-slate-100 bg-[#1e293bc6] px-6 py-2 rounded-full flex items-center gap-3 mt-5 max-w-64 justify-center mx-auto md:mx-0">
+                <span className="text-2xl">
+                  <FiPhoneCall />
                 </span>
-              ) : (
-                <span>
-                  Best <br /> Quality Dental Treatment
-                </span>
-              )}
-            </h1>
-            <p className="text-slate-200">
-              {lang ? (
-                <span className="">
-                  গত অর্ধ-যুগ ধরে আমরাই দিয়ে আসছি বাংলাদেশের সেরা ডেন্টাল সেবা।
-                </span>
-              ) : (
-                'We have been providing the best dental services in Bangladesh for the past half-century.'
-              )}
-            </p>
-            <button className="text-xl font-bold text-slate-100 bg-[#1e293bc6] px-6 py-2 rounded-full">
-              {lang ? <span>০১৪০১-৫৩২০১৮</span> : '01401-532018'}
-            </button>
+                {lang ? <span>০১৪০১-৫৩২০১৮</span> : '01401-532018'}
+              </span>
+            </div>
           </div>
-          <div className="min-h-64 w-1/2 max-w-[450px] ml-auto border-2 border-slate-200 bg-[#ffffffbd] rounded-md text-slate-800 p-4">
+          <div className="min-h-64 hidden md:block w-full md:w-1/2 max-w-[450px] ml-auto border-2 border-slate-200 bg-[#ffffffbd] rounded-md text-slate-800 p-4">
             <h1 className="text-4xl text-center font-bold pb-3">
               {lang ? (
                 <span className="bang text-3xl">অ্যাপয়েন্টমেন্ট নিন</span>
@@ -50,6 +58,20 @@ const Home = () => {
           </div>
         </div>
       </div>
+    <div className='w-11/12 mx-auto'>
+        <div className="min-h-64 my-5 md:hidden w-full border-2 border-slate-200 bg-[#ffffffbd] rounded-md text-slate-800 p-4">
+          <h1 className="text-4xl text-center font-bold pb-3">
+            {lang ? (
+              <span className="bang text-3xl">অ্যাপয়েন্টমেন্ট নিন</span>
+            ) : (
+              '  Make Appointment'
+            )}
+          </h1>
+  
+          {/* Appointment form======= */}
+          <BannerAppiontmentFrom />
+        </div>
+    </div>
     </div>
   );
 };
