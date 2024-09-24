85f1ac384487da0a1161532f7a1efb2f84856433
Md Ataullah <ataullahm100@gmail.com>
2024-09-25 00:49:19 +0600

    Google map section add

--- Client-Side/src/pages/About/About.jsx
+++ Client-Side/src/pages/About/About.jsx
@@ -8,6 +8,9 @@ const About = () => {
       {/* Google map link */}
       <div className="grid grid-cols-1 lg:grid-cols-2 w-full">
         <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
