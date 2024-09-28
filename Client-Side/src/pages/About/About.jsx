ff71c3ab2b2b1859db827a90a6b7c6806232e22b
Md Ataullah <ataullahm100@gmail.com>
2024-09-28 23:46:16 +0600

    Location Heading add

--- Client-Side/src/pages/About/About.jsx
+++ Client-Side/src/pages/About/About.jsx
@@ -20,42 +20,6 @@ const About = () => {
             className="shadow-lg rounded-lg"
           />
         </div>
-        <div className="flex justify-center">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="shadow-lg rounded-lg"
-          />
-        </div>
-        <div className="flex justify-center">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="shadow-lg rounded-lg"
-          />
-        </div>
-        <div className="flex justify-center">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="shadow-lg rounded-lg"
-          />
-        </div>
       </div>
     </div>
   );
