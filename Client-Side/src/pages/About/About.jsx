f72d687546992717802c587bb573c08600654cd7
Md Ataullah <ataullahm100@gmail.com>
2024-09-28 23:43:26 +0600

    Location section adding style

--- Client-Side/src/pages/About/About.jsx
+++ Client-Side/src/pages/About/About.jsx
@@ -1,12 +1,12 @@
 const About = () => {
   return (
     <div>
-      <h1 className="text-6xl font-bold text-center py-3 my-52">
+      <h1 className="text-5xl font-bold text-center py-3 mt-20">
         This is About Section.
       </h1>
 
       {/* Google map link */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 w-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-5 max-w-screen-xl mx-auto my-12">
         <div className="flex justify-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
@@ -19,7 +19,6 @@ const About = () => {
             className="shadow-lg rounded-lg"
           />
         </div>
-
         <div className="flex justify-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
