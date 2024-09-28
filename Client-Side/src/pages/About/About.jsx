bff3f842f6dc54736222c6a52a3a4ee7d2612abb
Md Ataullah <ataullahm100@gmail.com>
2024-09-28 23:47:46 +0600

    Heading add all location

--- Client-Side/src/pages/About/About.jsx
+++ Client-Side/src/pages/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       {/* Google map link */}
       <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-5 max-w-screen-xl mx-auto my-12">
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold mb-4">Jatrabari Branch</h1>
+          <h1 className="text-4xl font-bold mb-4">Mirpur Branch</h1>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
             width="100%"
@@ -34,7 +34,7 @@ const About = () => {
           />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold mb-4">Jatrabari Branch</h1>
+          <h1 className="text-4xl font-bold mb-4">Barishal Branch</h1>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
             width="100%"
@@ -47,7 +47,7 @@ const About = () => {
           />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold mb-4">Jatrabari Branch</h1>
+          <h1 className="text-4xl font-bold mb-4">Dawatola Branch</h1>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.73411920286!2d90.42842857353489!3d23.71682629006788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9001cf637c3%3A0x9fc68e9cab7e160c!2sSmile%20Dental%20Care%20-%20Saydabad%20-%20Jatrabari%20Branch!5e1!3m2!1sen!2sbd!4v1727278476402!5m2!1sen!2sbd"
             width="100%"
