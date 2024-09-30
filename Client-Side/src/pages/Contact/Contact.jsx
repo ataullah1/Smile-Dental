81bd10f3a79bcf896caea1d3670cc0c48bbca347
Md Ataullah <ataullahm100@gmail.com>
2024-10-01 00:36:13 +0600

    Contact page setup

--- Client-Side/src/pages/Contact/Contact.jsx
+++ Client-Side/src/pages/Contact/Contact.jsx
@@ -2,6 +2,11 @@ const Contact = () => {
   return (
     <div>
       <h1>Hello Contact us page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        veritatis libero non similique molestias tempore sapiente cumque totam,
+        reprehenderit exercitationem?
+      </p>
     </div>
   );
 };
