723d50881b2d56272e074375a94eaa6f60ac3940
Md Ataullah <ataullahm100@gmail.com>
2024-10-03 22:12:47 +0600

    Contact form adding

--- Client-Side/src/pages/Contact/Contact.jsx
+++ Client-Side/src/pages/Contact/Contact.jsx
@@ -1,17 +1,19 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Hello Contact us page</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
-        veritatis libero non similique molestias tempore sapiente cumque totam,
-        reprehenderit exercitationem?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
-        veritatis libero non similique molestias tempore sapiente cumque totam,
-        reprehenderit exercitationem?
-      </p>
+      <div>
+        <h1>Hello Contact us page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+          veritatis libero non similique molestias tempore sapiente cumque
+          totam, reprehenderit exercitationem?
+        </p>
+      </div>
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
     </div>
   );
 };
