c016fcbe26da0d8cbe62753d6203ad17ffeaee3d
Md Ataullah <ataullahm100@gmail.com>
2024-10-23 23:08:59 +0600

    Header Part

--- Client-Side/src/components/Header.jsx/Header.jsx
+++ Client-Side/src/components/Header.jsx/Header.jsx
@@ -2,7 +2,8 @@ const Header = () => {
   return (
     <div>
       <h1 className="text-5xl text-center font-bold py-5">
-        This is Header Section
+        This is Header Section This is Header Section This is Header Section
+        This is Header Section This is Header Section This is Header Section
       </h1>
     </div>
   );
