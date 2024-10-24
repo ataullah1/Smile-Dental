2e633989fbbf14dd57a94ff68e1106585fea06f5
Md Ataullah <ataullahm100@gmail.com>
2024-10-24 22:05:26 +0600

    header section

--- Client-Side/src/components/Header.jsx/Header.jsx
+++ Client-Side/src/components/Header.jsx/Header.jsx
@@ -4,6 +4,12 @@ const Header = () => {
       <h1 className="text-5xl text-center font-bold py-5">
         This is Header Section
       </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sunt
+        tempore quos! Repellendus optio porro fugiat numquam quisquam doloribus
+        assumenda, consequatur velit, nesciunt dolorem, excepturi eligendi
+        veritatis hic fugit magni.
+      </p>
     </div>
   );
 };
