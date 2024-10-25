5686141f84b36edeee62039bf6451709e553a546
Md Ataullah <ataullahm100@gmail.com>
2024-10-26 00:11:18 +0600

    .

--- Client-Side/src/components/Header.jsx/Header.jsx
+++ Client-Side/src/components/Header.jsx/Header.jsx
@@ -11,45 +11,6 @@ const Header = () => {
         veritatis hic fugit magni.
       </p>
     </div>
-const Header = () => {
-  return (
-    <div>
-      <h1 className="text-5xl text-center font-bold py-5">
-        This is Header Section
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sunt
-        tempore quos! Repellendus optio porro fugiat numquam quisquam doloribus
-        assumenda, consequatur velit, nesciunt dolorem, excepturi eligendi
-        veritatis hic fugit magni.
-      </p>
-    </div>
-const Header = () => {
-  return (
-    <div>
-      <h1 className="text-5xl text-center font-bold py-5">
-        This is Header Section
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sunt
-        tempore quos! Repellendus optio porro fugiat numquam quisquam doloribus
-        assumenda, consequatur velit, nesciunt dolorem, excepturi eligendi
-        veritatis hic fugit magni.
-      </p>
-    </div>
-const Header = () => {
-  return (
-    <div>
-      <h1 className="text-5xl text-center font-bold py-5">
-        This is Header Section
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sunt
-        tempore quos! Repellendus optio porro fugiat numquam quisquam doloribus
-        assumenda, consequatur velit, nesciunt dolorem, excepturi eligendi
-        veritatis hic fugit magni.
-      </p>
-    </div>
   );
 };
 
