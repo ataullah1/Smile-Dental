e926ac2dd0b51a7fb5766b9b5d64d243123e4cec
Md Ataullah <ataullahm100@gmail.com>
2024-10-28 19:54:21 +0600

    .

--- Client-Side/src/components/MiddleBanner/MiddleBanner.jsx
+++ Client-Side/src/components/MiddleBanner/MiddleBanner.jsx
@@ -33,6 +33,87 @@ const MiddleBanner = () => {
                   "Mirpur Branch"
                 )}
               </p>
+      className="relative py-12 w-full bg-cover bg-no-repeat bg-center mb-12 overflow-hidden bg-fixed"
+      style={{
+        backgroundImage: `url('https://i.ibb.co/SQjYDFK/dentall.jpg')`,
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-pClr opacity-85" />
+
+      {/* Content */}
+      <div className="relative w-11/12 mx-auto text-slate-100 text-center flex items-center justify-between flex-col lg:flex-row gap-5">
+        <div className="w-full lg:w-[70%]">
+          <h1 className="text-2xl border-b-2 border-slate-100 inline-block text-center font-bold mb-8">
+            {lang ? (
+              <span className="bang">যোগাযোগের নম্বর</span>
+            ) : (
+              "Contact Numbers"
+            )}
+          </h1>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-y-10">
+            <div className="bg-[#ffffff3f] border-2 border-slate-100 rounded-md p-3">
+              <p className="text-xl font-bold">
+                {lang ? (
+                  <span className="bang">মিরপুর শাখা</span>
+                ) : (
+                  "Mirpur Branch"
+                )}
+              </p>
+      className="relative py-12 w-full bg-cover bg-no-repeat bg-center mb-12 overflow-hidden bg-fixed"
+      style={{
+        backgroundImage: `url('https://i.ibb.co/SQjYDFK/dentall.jpg')`,
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-pClr opacity-85" />
+
+      {/* Content */}
+      <div className="relative w-11/12 mx-auto text-slate-100 text-center flex items-center justify-between flex-col lg:flex-row gap-5">
+        <div className="w-full lg:w-[70%]">
+          <h1 className="text-2xl border-b-2 border-slate-100 inline-block text-center font-bold mb-8">
+            {lang ? (
+              <span className="bang">যোগাযোগের নম্বর</span>
+            ) : (
+              "Contact Numbers"
+            )}
+          </h1>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-y-10">
+            <div className="bg-[#ffffff3f] border-2 border-slate-100 rounded-md p-3">
+              <p className="text-xl font-bold">
+                {lang ? (
+                  <span className="bang">মিরপুর শাখা</span>
+                ) : (
+                  "Mirpur Branch"
+                )}
+              </p>
+      className="relative py-12 w-full bg-cover bg-no-repeat bg-center mb-12 overflow-hidden bg-fixed"
+      style={{
+        backgroundImage: `url('https://i.ibb.co/SQjYDFK/dentall.jpg')`,
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-pClr opacity-85" />
+
+      {/* Content */}
+      <div className="relative w-11/12 mx-auto text-slate-100 text-center flex items-center justify-between flex-col lg:flex-row gap-5">
+        <div className="w-full lg:w-[70%]">
+          <h1 className="text-2xl border-b-2 border-slate-100 inline-block text-center font-bold mb-8">
+            {lang ? (
+              <span className="bang">যোগাযোগের নম্বর</span>
+            ) : (
+              "Contact Numbers"
+            )}
+          </h1>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-y-10">
+            <div className="bg-[#ffffff3f] border-2 border-slate-100 rounded-md p-3">
+              <p className="text-xl font-bold">
+                {lang ? (
+                  <span className="bang">মিরপুর শাখা</span>
+                ) : (
+                  "Mirpur Branch"
+                )}
+              </p>
               <a
                 href="tel:+8801401-532018"
                 className="text-2xl font-bold text-slate-100 flex items-center gap-3 mt-2 justify-center mx-auto md:mx-0"
