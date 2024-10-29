8ca8519ec7a2bfc21e13d2ab7c83aff7bb125cdd
Md Ataullah <ataullahm100@gmail.com>
2024-10-30 00:01:33 +0600

    Some changes

--- Client-Side/src/components/MiddleBanner/SecondMiddleBanner.jsx
+++ Client-Side/src/components/MiddleBanner/SecondMiddleBanner.jsx
@@ -1,11 +1,14 @@
-
-
 const SecondMiddleBanner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis ab
+        temporibus est accusantium pariatur nobis laboriosam sit voluptatum, nam
+        quis blanditiis in. Consequatur ex similique excepturi, architecto ipsam
+        quis nihil.
+      </p>
+    </div>
+  );
 };
 
 export default SecondMiddleBanner;
