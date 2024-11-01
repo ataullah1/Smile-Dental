fa09516bcb025045c88f788ee3c7116ca66fe9b3
Md Ataullah <ataullahm100@gmail.com>
2024-11-02 00:29:53 +0600

    Sone text added

--- Client-Side/src/components/MiddleBanner/SecondMiddleBanner.jsx
+++ Client-Side/src/components/MiddleBanner/SecondMiddleBanner.jsx
@@ -2,9 +2,24 @@ const SecondMiddleBanner = () => {
   return (
     <div>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-        officia corporis provident in corrupti repellat maxime dolorum natus
-        repudiandae molestias!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Tenetur Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit. Tenetur Lorem, ipsum dolor
+        sit amet consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tenetur officia corporis provident in
+        corrupti repellat maxime dolorum natus repudiandae molestias!
       </p>
     </div>
   );
